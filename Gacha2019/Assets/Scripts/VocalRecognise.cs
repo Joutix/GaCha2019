60f24b69ad5b1e13dc0f9c0db30462ef5cac18a5
Sabri <sabribenbrahim@outlook.com>
2019-04-13 16:13:38 +0200

    Update VocalRecognise
    Possibilité de reconnaître des mots et réalisé des actions

--- Gacha2019/Assets/Scripts/VocalRecognise.cs
+++ Gacha2019/Assets/Scripts/VocalRecognise.cs
@@ -1,18 +1,49 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
+using UnityEngine.Windows.Speech;
 
-public class ReconnaissanceVocal : MonoBehaviour
+public class VocalRecognise : MonoBehaviour
 {
+	private KeywordRecognizer keywordRecognizer;
+	private Dictionary<string, Action> actions = new Dictionary<string, Action>();
+
     // Start is called before the first frame update
     void Start()
     {
-        
-    }
+		actions.Add("avance", Forward);
+		actions.Add("up", Up);
+		actions.Add("down", Down);
+		actions.Add("back", Back);
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+		keywordRecognizer = new KeywordRecognizer(actions.Keys.ToArray());
+		keywordRecognizer.OnPhraseRecognized += RecognizedSpeech;
+		keywordRecognizer.Start();
+
+	}
+
+	private void RecognizedSpeech(PhraseRecognizedEventArgs speech)
+	{
+		Debug.Log(speech.text);
+		actions[speech.text].Invoke();
+	}
+
+    private void Forward()
+	{
+		transform.Translate(1, 0, 0);
+	}
+	private void Back()
+	{
+		transform.Translate(-1, 0, 0);
+	}
+	private void Up()
+	{
+		transform.Translate(0, 1, 0);
+	}
+	private void Down()
+	{
+		transform.Translate(0, -1, 0);
+	}
 }
