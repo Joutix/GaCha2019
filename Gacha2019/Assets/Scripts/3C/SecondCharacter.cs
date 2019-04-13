7a7296b466cad964e3b11921acba10f29de1adf5
Adrien Masson <ma.masson.adrien@gmail.com>
2019-04-13 23:44:11 +0200

    Fixed rotation
    works properly now i hope

--- Gacha2019/Assets/Scripts/3C/SecondCharacter.cs
+++ Gacha2019/Assets/Scripts/3C/SecondCharacter.cs
@@ -6,29 +6,31 @@ public class SecondCharacter : MonoBehaviour
 {
 
     [SerializeField] public float m_DistanceToBody = 1.2f;
+    [SerializeField] private GameObject m_OrbitalPrefab = null;
 
-    [SerializeField] private GameObject m_Prefab = null;
+    private GameObject m_Orbital = null;
 
 
-    private Vector3 m_Offset = new Vector3(1, 0, 0);
+    private Vector3 m_OffsetVector = new Vector3(1, 0, 0);
 
     public void TurnAroundRoot(float m_Right, float m_Up)
     {
-        m_Offset = (m_Right * Vector3.right + m_Up * Vector3.forward).normalized;
-        m_Prefab.transform.position = transform.position + m_Offset *m_DistanceToBody;
+        m_OffsetVector = (m_Right * Vector3.right + m_Up * Vector3.forward).normalized;
+        m_Orbital.transform.position = transform.position + m_OffsetVector * m_DistanceToBody;
     }
 
     // Start is called before the first frame update
     void Start()
     {
-
+        m_Orbital = Instantiate<GameObject>(m_OrbitalPrefab.gameObject);
+        m_Orbital.transform.position = transform.position + m_OffsetVector * m_DistanceToBody;
     }
 
     // Update is called once per frame
     void Update()
     {
         //update position of the orbital
-        m_Prefab.transform.position = transform.position + m_Offset * m_DistanceToBody;
+        m_Orbital.transform.position = transform.position + m_OffsetVector * m_DistanceToBody;
 
         if (Input.GetKeyDown(KeyCode.O))
         {
