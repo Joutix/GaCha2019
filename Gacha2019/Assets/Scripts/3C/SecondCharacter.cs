e0787bdfefada818fe2c96f12bf8af07a227f04f
Adrien Masson <ma.masson.adrien@gmail.com>
2019-04-13 22:45:42 +0200

    Push Second Character
    prefabs for second, and first +second and 2nd character can turn around first character in 4 directions (left, up, right, down)

---
+++ Gacha2019/Assets/Scripts/3C/SecondCharacter.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SecondCharacter : MonoBehaviour
+{
+
+    public float m_DistanceToBody = 1.2f;
+
+    [SerializeField] private GameObject m_Prefab = null;
+
+
+    private Vector3 m_Offset = new Vector3(1, 0, 0);
+
+    public void TurnAroundRoot(float m_Right, float m_Up)
+    {
+        m_Offset = (m_Right * Vector3.right + m_Up * Vector3.forward).normalized;
+        m_Prefab.transform.position = transform.position + m_Offset *m_DistanceToBody;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //update position of the orbital
+        m_Prefab.transform.position = transform.position + m_Offset * m_DistanceToBody;
+
+        if (Input.GetKeyDown(KeyCode.O))
+        {
+            TurnAroundRoot(0, 1);
+        }
+
+        else if (Input.GetKeyDown(KeyCode.L))
+        {
+            TurnAroundRoot(0, -1);
+        }
+
+        else if (Input.GetKeyDown(KeyCode.K))
+        {
+            TurnAroundRoot(-1, 0);
+        }
+        else if (Input.GetKeyDown(KeyCode.M))
+        {
+            TurnAroundRoot(1, 0);
+        }
+    }
+}
