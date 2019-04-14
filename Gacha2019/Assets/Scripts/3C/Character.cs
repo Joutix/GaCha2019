b95d6f7ee1bddcfbfb56c729ecf2e96c9a29fa79
Sabri <sabribenbrahim@outlook.com>
2019-04-14 17:32:41 +0200

    Upgrade Character & TestMicro

--- Gacha2019/Assets/Scripts/3C/Character.cs
+++ Gacha2019/Assets/Scripts/3C/Character.cs
@@ -20,8 +20,8 @@ public class Character : Entity
 
     public void Teleport(GameGrid grid, int _DeltaRow, int _DeltaColumn)
     {
-
-    }
+		MoveTo(_DeltaRow, _DeltaColumn);
+	}
 
     #endregion
 
@@ -76,14 +76,14 @@ public class Character : Entity
         //works for now  as this is a teleport and it's instantaneous
         //DELETE THIS LATER IF PLAYER DOESNT TP TO OTHER CELLS
 
-        grid.GetGridCellAt(m_CurrentRow, m_CurrentColumn).OnCellExited(this);
         grid.GetGridCellAt(_RowDestination, _ColumnDestination).OnCellEntered(this);
 
         m_CurrentRow = _RowDestination;
         m_CurrentColumn = _ColumnDestination;
         m_CanMove = false;
         m_MovementTimer = Time.time;
-    }
+		grid.GetGridCellAt(m_CurrentRow, m_CurrentColumn).OnCellExited(this);
+	}
     #endregion
 
 
