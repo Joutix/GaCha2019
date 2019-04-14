a3cfce7eabeb070ce25529dda8a93a7239769997
Adrien Masson <ma.masson.adrien@gmail.com>
2019-04-14 18:55:46 +0200

    added dictionnary of grid/list<Enemy> with add/remove function

--- Gacha2019/Assets/Scripts/GameManager.cs
+++ Gacha2019/Assets/Scripts/GameManager.cs
@@ -1,4 +1,6 @@
 ﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
 using MyUtilities.DesignPatterns;
 
 public class GameManager : Singleton<GameManager>
@@ -13,6 +15,33 @@ public class GameManager : Singleton<GameManager>
     {
         m_Character = _Character;
     }
+
+    //adds the given enemy to the manager, if it isn't already in its list
+    public void AddEnemyToManager(Enemy _enemyToAdd, GameGrid _grid)
+    {
+        if (_grid && _enemyToAdd)
+        {
+
+            if (!m_DictionnaryOfEnemies[_grid].Contains(_enemyToAdd))
+                m_DictionnaryOfEnemies[_grid].Add(_enemyToAdd);
+            //if (!m_ListOfEnemies.Contains(_enemyToAdd))
+            //    m_ListOfEnemies.Add(_enemyToAdd);
+            else { Debug.LogWarning("enemy to add is already in the list"); }
+        }
+    }
+
+    //removes the given enemy from the manager, if it is inside its list
+    public void RemoveEnemyFromManager(Enemy _enemyToRemove, GameGrid _grid)
+    {
+        if (_grid && _enemyToRemove)
+        {
+            if (m_DictionnaryOfEnemies[_grid].Contains(_enemyToRemove))
+                // if (m_ListOfEnemies.Contains(_enemyToAdd))
+                m_DictionnaryOfEnemies[_grid].Add(_enemyToRemove);
+            else { Debug.LogWarning("enemy to remove isn't in the list yet"); }
+        }
+    }
+
     #endregion
 
     #region Getters / Setters
@@ -36,5 +65,9 @@ public class GameManager : Singleton<GameManager>
     #region Attributes
     private GameGrid m_GameGrid = null;
     private Character m_Character = null;
+
+    //private List<Enemy> m_ListOfEnemies = new List<Enemy>();
+    private Dictionary<GameGrid, List<Enemy>> m_DictionnaryOfEnemies = new Dictionary<GameGrid, List<Enemy>>();
+
     #endregion
 }
