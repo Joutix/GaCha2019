b66da79b2b897c47ca4a72cb59d73c4edd196210
Sabri <sabribenbrahim@outlook.com>
2019-04-14 10:45:33 +0200

    Add CellTeleportation
    Ajout du script téléportation vers un cell et ajout d'une scène de test.

---
+++ Gacha2019/Assets/Scripts/CellTeleportation.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CellTeleportation : MonoBehaviour
+{
+	//[SerializeField]
+	//private GameObject m_DirToCell;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+	public void teleportation(GameObject m_DirToCell)
+	{
+		Vector3 pos = new Vector3();
+		pos.x = m_DirToCell.transform.position.x;
+		pos.z = m_DirToCell.transform.position.z;
+		pos.y = GameManager.Instance.Character.transform.localPosition.y;
+		GameManager.Instance.Character.transform.localPosition = pos;
+	}
+}
